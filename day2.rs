486c3a70f60253ccb44b04db36e2600dea3d0bc6
Tim Nimets <tymnim@gmail.com>
2023-12-02 21:30:00 -0500

    Day 2 | 2nd star

--- day2.rs
+++ day2.rs
@@ -6,10 +6,43 @@ fn main() {
     .expect("Should have been able to read the file");
   let lines = contents.split("\n");
   let sum: i32 = lines.into_iter()
-  .map(parse_line_part1)
+  .map(parse_line_part2)
   .sum();
 
   println!("SUM: {}", sum);
+}
+fn parse_line_part2(line: &str) -> i32 {
+  if let Some((_game, content)) = line.split_once(": ") {
+    let mut reds: Vec<i32> = Vec::new();
+    let mut greens: Vec<i32> = Vec::new();
+    let mut blues: Vec<i32> = Vec::new();
+    let draws = content.split("; ");
+    for draw in draws {
+      let balls = draw.split(", ");
+      for ball in balls {
+        if let Some((number, color)) = ball.split_once(" ") {
+          match color {
+            "red" => reds.push(number.parse::<i32>().unwrap()),
+            "green" => greens.push(number.parse::<i32>().unwrap()),
+            "blue" => blues.push(number.parse::<i32>().unwrap()),
+            _ => {}
+          }
+        }
+        else {
+          // error
+        }
+      }
+    }
+
+    return reds.iter().max().unwrap_or(&1)
+      * greens.iter().max().unwrap_or(&1)
+      * blues.iter().max().unwrap_or(&1);
+  }
+  else {
+    // do nothing;
+    println!("Failed Initial Parse");
+  }
+  return 0;
 }
 
 fn parse_line_part1(line: &str) -> i32 {
@@ -47,8 +80,6 @@ fn parse_line_part1(line: &str) -> i32 {
     // do nothing;
     println!("Failed Initial Parse");
   }
-
-
   return 0;
 }
 
