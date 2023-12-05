62f34910cad3097ba8fee74cba8fb889cca3e5df
Tim Nimets <tymnim@gmail.com>
2023-12-04 21:22:00 -0500

    Day 4 | part 2

--- day4.rs
+++ day4.rs
@@ -5,14 +5,36 @@ fn main() {
   let contents = fs::read_to_string(file_path)
     .expect("Should have been able to read the file");
   let lines = contents.split("\n");
-  let sum: u32 = lines.into_iter()
-  .map(parse_part_1)
-  .sum();
+  let games = lines.into_iter()
+  .map(parse_ticket);
+  // NOTE: part1
+  // .map(|count| {
+  //   if count > 0 {
+  //     let pp = u32::pow(2, (count - 1) as u32);
+  //     return pp;
+  //   }
+  //   return 0;
+  // })
+  // .sum();
 
-  println!("SUM: {}", sum);
+  let mut game_scores: Vec<(u32, u32)> = games.map(|score| (score, 1)).collect();
+  game_scores = game_scores[0..game_scores.len() - 1].to_vec();
+  let limit = game_scores.len();
+  for i in 0..limit {
+    let (score, tickets) = game_scores[i];
+    for j in 1..(score + 1) as usize {
+      let game = i + j;
+      if game < limit {
+        game_scores[game].1 += tickets
+      }
+    }
+  }
+  let sum: u32 = game_scores.iter().map(|(_, tickets)| tickets).sum();
+  println!("SUM: {sum}");
+
 }
 
-fn parse_part_1(line: &str) -> u32 {
+fn parse_ticket(line: &str) -> u32 {
   if let Some((_, numbers)) = line.split_once(": ") {
     if let Some((winning, mine)) = numbers.split_once(" | ") {
       let winning_numbers: Vec<&str> = winning.split(" ").filter(| number | number.len() > 0).collect();
@@ -22,12 +44,7 @@ fn parse_part_1(line: &str) -> u32 {
         .filter(|number| winning_numbers.contains(number))
         .map(|number| number.parse::<i32>().unwrap())
         .collect();
-      let count = my_winning.len();
-      if count > 0 {
-        let pp = u32::pow(2, (count - 1) as u32);
-        return pp;
-      }
-      return 0;
+      return my_winning.len() as u32;
     }
   }
   return 0;
